c8684af705fb9d91e02b3d2ad165fcf244fdbfea
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-06-03 17:48:29 +0800

    #19 Generate backend controllers

---
+++ spec/helpers/backend/products_helper_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Backend::ProductsHelper. For example:
+#
+# describe Backend::ProductsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe Backend::ProductsHelper, type: :helper do
+end
