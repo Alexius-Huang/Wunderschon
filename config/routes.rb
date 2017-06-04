f2a4eaff07ec7079a8b9486e974ed57e18074401
Maxwell <destinydragon85614@gmail.com>
2017-06-04 12:48:19 +0800

    #19 Add Backend Controllers and Routes (#22)
    * #19 Generate backend controllers
    
    * Add backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
   end
 
   resources :orders, path_names: { new: 'checkout' }
+
+  namespace :backend do
+    resources :categories, only: [:new, :edit, :update, :destroy]
+    resources :orders,     only: [:index, :show, :edit, :update, :destroy]
+    resources :products,   only: [:new, :edit, :update, :destroy]
+    resources :users
+  end
 end
