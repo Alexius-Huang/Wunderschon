462fa5c8afacb5ef91b82cdab12b5fe4739cfa51
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-06-07 22:58:04 +0800

    #12 Add I18n locale system

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Wunderschon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Taipei'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = 'zh-TW'
   end
 end
