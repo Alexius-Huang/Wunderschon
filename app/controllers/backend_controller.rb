cc713c80f3c995c7d269ef934265c6a821715bf1
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-06-08 21:03:14 +0800

    #37 Setting up Backend#main page

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
 class BackendController < ApplicationController
+  before_action :without_navbar
+  before_action :backend_mode
+
+  def main; end
+
+  private
+
+  def without_navbar
+    @without_navbar = true
+  end
+
+  def backend_mode
+    @backend = true
+  end
 end
--- app/helpers/backend_helper.rb
+++ app/helpers/backend_helper.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 module BackendHelper
+  def breadcrumb(i18n_prefix = 'dashboard.breadcrumb.', fullpath = request.fullpath)
+    path_arr = fullpath.split '/'
+    path_arr.shift if path_arr.first.empty?
+    path_arr.map { |path_segment| t(i18n_prefix + path_segment) }.join(' >> ')
+  end
 end
