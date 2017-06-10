94a42992fa1997d77c74b41f26730a6113bc3ddf
Maxwell <destinydragon85614@gmail.com>
2017-06-10 20:10:36 +0800

    #46 Add Backend::ProductsController#show, #edit, #update & #destroy UI & Actions (#48)
    * #46 Add Backend::ProductsController#show, #edit, #update & #destroy UI & Actions
    
    * Add navbar active class

--- app/controllers/backend/products_controller.rb
+++ app/controllers/backend/products_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class Backend::ProductsController < BackendController
+  before_action :find_product, only: %i(show edit update destroy)
   def index
     @products = Product.all
   end
@@ -17,9 +18,30 @@ class Backend::ProductsController < BackendController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @product.update(resource_params)
+      redirect_to backend_product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to backend_products_path
+  end
+
   private
 
   def resource_params
     params.require(:product).permit(:title, :category_id, :description, :price)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
